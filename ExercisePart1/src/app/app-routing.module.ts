8f884a12aaa3e2212d4adb4a713e0136a8c71de6
Droxinbox <jose.mcastaneda@alumnos.udg.mx>
2024-03-20 19:53:45 -0600

    Activity 05
    The Activity 05 is now completed and ready for review.

--- ExercisePart1/src/app/app-routing.module.ts
+++ ExercisePart1/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { MyFirstComponentComponent } from './my-first-component/my-first-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Home', component: AppComponent },
+  { path: 'MyFirstComponent', component: MyFirstComponentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
