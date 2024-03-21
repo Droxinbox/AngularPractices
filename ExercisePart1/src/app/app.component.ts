611475483997091e27e22f54a30c8eed9ae30534
Droxinbox <jose.mcastaneda@alumnos.udg.mx>
2024-03-21 09:30:05 -0600

    Activity 08
    The Activity 08 is now completed and ready for review.

--- ExercisePart1/src/app/app.component.ts
+++ ExercisePart1/src/app/app.component.ts
@@ -5,6 +5,15 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'ExercisePart1';
+
+  name: string = "";
+  showName: string = "";
+
+  shareName() {
+    this.showName = this.name;
+  }
+
 }
