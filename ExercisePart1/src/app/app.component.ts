ea55661cb91b7799f0000acb90e42607bbf4e6f9
Droxinbox <jose.mcastaneda@alumnos.udg.mx>
2024-03-26 10:53:26 -0600

    Activity 01 ver 2.0
    + Activity 01 has been converted into a component.
    + Unnecessary lines of code have been removed.

--- ExercisePart1/src/app/app.component.ts
+++ ExercisePart1/src/app/app.component.ts
@@ -6,36 +6,4 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 
-export class AppComponent {
-  title = 'ExercisePart1';
-
-  activeClass: boolean = false;
-  activeStyle: boolean = false;
-
-  red = {
-    'redText' : true
-  };
-
-  purple = {
-    'purpleText' : true
-  };
-
-  green = {
-    'greenText' : true
-  };
-
-  stateChangeClass() {
-    if (this.activeClass)
-      this.activeClass = false;
-    else
-      this.activeClass = true;
-  }
-
-  stateChangeStyle() {
-    if (this.activeStyle)
-      this.activeStyle = false;
-    else
-      this.activeStyle = true;
-  }
-
-}
+export class AppComponent {}
